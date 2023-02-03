093c9d2c9a5b603d4d8ff7d05494d135d8a266d8
Ben Cavins <ben.cavins@flatironschool.com>
2023-02-03 16:02:17 -0500

    pkmon pt1

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+  const [pkList, setPkList] = useState([])
+  const [searchQuery, setSearchQuery] = useState("")
+
+  useEffect(() => {
+    fetch('http://localhost:3001/pokemon')
+      .then(resp => resp.json())
+      .then(data => setPkList(data))
+  }, [])
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
       <PokemonForm />
       <br />
-      <Search />
+      <Search
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery} />
       <br />
-      <PokemonCollection />
+      <PokemonCollection
+        pkList={pkList}
+        setPkList={setPkList}
+        searchQuery={searchQuery} />
     </Container>
   );
 }
