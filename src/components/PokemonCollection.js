8a1e74b941f253adc17015ef320c0883ed3d99f1
Ben Cavins <bencavins@gmail.com>
2024-12-12 13:57:57 -0500

    add pokemon to the page

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,10 +2,13 @@ import React from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
-function PokemonCollection() {
+function PokemonCollection({ pks }) {
+
   return (
     <Card.Group itemsPerRow={6}>
-      <h1>Hello From Pokemon Collection</h1>
+      {pks.map(pk => {
+        return <PokemonCard key={pk.id} {...pk} />
+      })}
     </Card.Group>
   );
 }
